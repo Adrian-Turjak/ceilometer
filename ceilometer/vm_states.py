872f8fd1a79eac85ce6d9b9fc12292d3a38d0b63
Adrian-Turjak <adriant@catalyst.net.nz>
2014-01-17 10:16:04 +1300

    Update vm_states.py
    Updating documentation.

--- ceilometer/vm_states.py
+++ ceilometer/vm_states.py
@@ -1,8 +1,8 @@
 
-
 """"Mapping of Nova VM states to ints for use in metering. """
 # It might be better to build a query for this list in
 # the NoveClient rather than store it here.
+# Or import vm_states and use the names defined there.
 states = {'active': 1,
           'building': 2,
           'paused': 3,
