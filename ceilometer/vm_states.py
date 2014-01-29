d36ab013527a7320747b3d85a66a005979e0c47e
adriant <adriant@catalyst.net.nz>
2014-01-30 12:40:28 +1300

    re-added import for vm states from nova

--- ceilometer/vm_states.py
+++ ceilometer/vm_states.py
@@ -1,17 +1,17 @@
+from nova.compute import vm_states as vms
 
 """"Mapping of Nova VM states to ints for use in metering. """
 # It might be better to build a query for this list in
 # the NoveClient rather than store it here.
-# Or import vm_states and use the names defined there.
-states = {'active': 1,
-          'building': 2,
-          'paused': 3,
-          'suspended': 4,
-          'stopped': 5,
-          'rescued': 6,
-          'resized': 7,
-          'soft_deleted': 8,
-          'deleted': 9,
-          'error': 10,
-          'shelved': 11,
-          'shelved_offloaded': 12}
+states = {vms.ACTIVE: 1,
+          vms.BUILDING: 2,
+          vms.PAUSED: 3,
+          vms.SUSPENDED: 4,
+          vms.STOPPED: 5,
+          vms.RESCUED: 6,
+          vms.RESIZED: 7,
+          vms.SOFT_DELETED: 8,
+          vms.DELETED: 9,
+          vms.ERROR: 10,
+          vms.SHELVED: 11,
+          vms.SHELVED_OFFLOADED: 12}
