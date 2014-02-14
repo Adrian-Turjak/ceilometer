835de92f1c36471011c2836faef21f8ba4234afc
adriant <adriant@catalyst.net.nz>
2014-02-14 16:23:41 +1300

    adding a flavor metric pollster

---
+++ ceilometer/compute/pollsters/flavor.py
@@ -0,0 +1,20 @@
+from ceilometer.compute import plugin
+from ceilometer.compute.pollsters import util
+# from ceilometer.compute.virt import inspector as virt_inspector
+from ceilometer.openstack.common.gettextutils import _  # noqa
+from ceilometer.openstack.common import log
+from ceilometer import sample
+LOG = log.getLogger(__name__)
+
+
+class FlavorPollster(plugin.ComputePollster):
+
+    def get_samples(self, manager, cache, instance):
+
+        yield util.make_sample_from_instance(
+            instance,
+            name='flavor',
+            type=sample.TYPE_GAUGE,
+            unit='state',
+            volume=instance.flavor['id'],
+        )
